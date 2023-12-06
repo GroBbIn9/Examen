7e1c4a7a99d6ad4fd49ecb5b964d2546f276394c
iamskyline <1234fil1234@gmail.com>
2023-12-06 16:25:37 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using EXZ.Car;
+
+namespace Variant3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OutputJson();
+        }
+
+        public void OutputJson()
+
+        {
+            try
+            {
+
+                string json = System.IO.File.ReadAllText(@"Kniga3.json");
+
+
+                Information.ItemsSource = JsonConvert.DeserializeObject<List<UserZaivka>>(json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
